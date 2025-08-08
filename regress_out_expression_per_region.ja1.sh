e4b048dca424b1837add0451d2c39e90e1b62139
Tadeo Spencer <88002862+tcspencer01@users.noreply.github.com>
2025-08-08 15:16:31 -0700

    Update regress_out_expression_per_region.ja1.sh

--- regress_out_expression_per_region.ja1.sh
+++ regress_out_expression_per_region.ja1.sh
@@ -8,7 +8,6 @@ file_suffix="_phenotype_pcs_regressed_out.txt" #everything up till ".txt"
 rm $ja #remove old job file
 
 pcs=(1 2 3 4 5)
-#pcs=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 
 for num_pcs in ${pcs[*]}
 do
